9ea66a4b99a463879eef5c0d8b29bcf540edc24a
Tendai Muzwidzwa <tendai.r.muz@hotmail.com>
2014-06-21 17:52:02 +0100

    Mod3

--- railsgirls-galway-2014/config/routes.rb
+++ railsgirls-galway-2014/config/routes.rb
@@ -1,4 +1,9 @@
 RailsgirlsGalway2014::Application.routes.draw do
+  resources :comments
+
+  resources :places
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
